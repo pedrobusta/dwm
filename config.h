3f485b6dcc7c3030066e680ecffa4142a235b8e5
pedrobusta <pedrobustamanted@gmail.com>
2024-03-08 20:17:52 +0100

    stable

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define BROWSER "firefox"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 9;        /* gaps between windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -34,7 +34,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "discord",    NULL,       NULL,     1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
